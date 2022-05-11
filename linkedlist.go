24e66f02d82134fa0b09b885141b2be94a6d5dba
Justin Johnson <booleangate@gmail.com>
2022-05-10 23:33:58 -0700

    insert

--- linkedlist.go
+++ linkedlist.go
@@ -13,34 +13,62 @@ type llNode[T any] struct {
 	val  T
 }
 
+func newNodeChain[T any](vals []T) (head, tail *llNode[T]) {
+	head = &llNode[T]{val: vals[0]}
+	tail = head
+	vals = vals[1:]
+	for i := range vals {
+		tail.next = &llNode[T]{val: vals[i]}
+		tail = tail.next
+	}
+	return head, tail
+}
+
 func (l *LinkedList[T]) Append(vals ...T) {
 	if len(vals) == 0 {
 		return
 	}
 
-	chain := &llNode[T]{val: vals[0]}
-	curr := chain
-	valLen := len(vals)
-	vals = vals[1:]
-	for i := range vals {
-		curr.next = &llNode[T]{val: vals[i]}
-		curr = curr.next
-	}
-
 	if l.head == nil {
-		l.head = chain
+		l.head, _ = newNodeChain(vals)
 	} else {
 		tail := l.head
 		for tail.next != nil {
 			tail = tail.next
 		}
-		tail.next = chain
+		tail.next, _ = newNodeChain(vals)
 	}
-	l.len += valLen
+	l.len += len(vals)
+}
+
+func (l *LinkedList[T]) Insert(idx int, vals ...T) error {
+	if len(vals) == 0 {
+		return nil
+	}
+
+	// Always allow insert at 0, even when empty
+	if idx == 0 {
+		head, tail := newNodeChain(vals)
+		tail.next = l.head
+		l.head = head
+	} else {
+		node, prev, err := l.node(idx)
+		if err != nil {
+			return err
+		}
+
+		head, tail := newNodeChain(vals)
+		prev.next = head
+		tail.next = node
+
+	}
+	l.len += len(vals)
+
+	return nil
 }
 
 func (l *LinkedList[T]) At(idx int) (T, error) {
-	node, err := l.node(idx)
+	node, _, err := l.node(idx)
 	if err != nil {
 		return zeroValue[T](), err
 	}
@@ -102,24 +130,25 @@ func (l *LinkedList[T]) IteratorF() IteratorF[T] {
 	}
 }
 
-func (l *LinkedList[T]) node(idx int) (*llNode[T], error) {
+func (l *LinkedList[T]) node(idx int) (node, prev *llNode[T], err error) {
 	if l.len == 0 {
-		return nil, fmt.Errorf("index %d is out of bounds, list is empty", idx)
+		return nil, nil, fmt.Errorf("index %d is out of bounds, list is empty", idx)
 	}
 	if idx < 0 || idx >= l.len {
-		return nil, fmt.Errorf("index %d is out of bounds [0, %d)", idx, l.len)
+		return nil, nil, fmt.Errorf("index %d is out of bounds [0, %d)", idx, l.len)
 	}
 
 	i := 0
-	n := l.head
-	for ; n != nil; n = n.next {
+	node = l.head
+	for node != nil {
 		if i == idx {
 			break
 		}
+		prev, node = node, node.next
 		i++
 	}
 
-	return n, nil
+	return node, prev, nil
 }
 
 type llIterator[T any] struct {
